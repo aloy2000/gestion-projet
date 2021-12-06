6c8d141506e2d9a7b1e86a01c49c98f36860d144
aloy2000 <ramanandraibenirina@gmail.com>
2021-12-06 20:00:37 +0300

    test branch

---
+++ client/src/screens/homepage/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import background from '.'
+
+const HomePage = () => {
+    return (
+        <div className='homepage'>
+            <h1>
+                Baobab Agency
+            </h1>
+            <div className="images">
+                <div>image 1</div>
+                <div>image 2</div>
+                <div>image 3</div>
+            </div>
+            <div className="navigation">
+                <div>
+                    navigation 1
+                </div>
+                <div>
+                    navigation 2
+                </div>
+                <div>
+                    navigation 3
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
